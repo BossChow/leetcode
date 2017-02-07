2c7536db3aece2a6baad88fab2280b919f32083c
kian <zhuce8270@163.com>
2017-02-07 18:57:15 +0800

    Hamming Distance

---
+++ Hamming_Distance/Solution.java
@@ -0,0 +1,27 @@
+
+/**
+ * The Hamming distance between two integers is the number of 
+ * positions at which the corresponding bits are different.
+ *
+ * Input: x = 1, y = 4
+ * Output: 2
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        int testValue_x = 1;
+        int testValue_y = 4;
+
+        System.out.println("Test Value: " + String.valueOf(testValue_x) + ", " + String.valueOf(testValue_y));
+
+        System.out.println("Result: " + String.valueOf(hammingDistance(testValue_x, testValue_y)));
+    }
+
+    public static int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
